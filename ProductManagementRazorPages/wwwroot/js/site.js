d4225fdba71ecd1444e8e83ce3eaf41bb2d8af4a
Shinjuuichi <sinlthai570@gmail.com>
2025-05-07 19:36:25 +0700

    lib lỏdd

--- ProductManagementRazorPages/wwwroot/js/site.js
+++ ProductManagementRazorPages/wwwroot/js/site.js
@@ -2,3 +2,14 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+"use strict";
+
+var connection = new signalR.HubConnectionBuilder().withUrl("/signalRServer").build();
+
+connection.on("LoadAllItems", function () {
+    location.href = '/Products/Index';
+});
+
+connection.start().catch(function (err) {
+    return console.error(err.toString());
+});
